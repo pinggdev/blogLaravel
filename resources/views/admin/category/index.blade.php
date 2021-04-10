078ff6dd6362ef2b7548c17d1591d42a2a3753fc
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-04-10 16:57:25 +0700

    pagination dan penomoran kategori

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -10,14 +10,17 @@
             </tr>
         </thead>
         <tbody>
-                @foreach ($category as $result)
+                @foreach ($category as $result => $hasil)
                     <tr>
-                        <td></td>
-                        <td>{{ $result->name }}</td>
-                        <td><a href="" class="btn btn-primary btn-sm">Edit</a></td>
-                        <td><a href="" class="btn btn-danger btn-sm">Delete</a></td>
+                        <td>{{ $result + $category->firstitem() }}</td>
+                        <td>{{ $hasil->name }}</td>
+                        <td>
+                            <a href="" class="btn btn-primary btn-sm">Edit</a>
+                            <a href="" class="btn btn-danger btn-sm">Delete</a>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
+        {{ $category->links() }}
 @endsection
