4343f31f9f986e63b1cfe8ff58e94d1c12171ba9
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-04-14 15:07:13 +0700

    login pada laravel dan auth

--- resources/views/template_backend/home.blade.php
+++ resources/views/template_backend/home.blade.php
@@ -40,8 +40,13 @@
                 <i class="far fa-user"></i> Profile
               </a>
               <div class="dropdown-divider"></div>
-              <a href="#" class="dropdown-item has-icon text-danger">
-                <i class="fas fa-sign-out-alt"></i> Logout
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                 @csrf
+              </form>
+              <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
               </a>
             </div>
           </li>
--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
-})->name('home');
-
-Route::resource('/category', 'CategoryController');
-Route::resource('/tag', 'TagController');
-
-Route::get('/post/tampil_hapus', 'PostController@tampil_hapus')->name('post.tampil_hapus');
-Route::get('/post/restore/{id}', 'PostController@restore')->name('post.restore');
-Route::delete('/post/kill/{id}', 'PostController@kill')->name('post.kill');
-
-Route::resource('/post', 'PostController');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('/category', 'CategoryController');
+    Route::resource('/tag', 'TagController');
+    
+    Route::get('/post/tampil_hapus', 'PostController@tampil_hapus')->name('post.tampil_hapus');
+    Route::get('/post/restore/{id}', 'PostController@restore')->name('post.restore');
+    Route::delete('/post/kill/{id}', 'PostController@kill')->name('post.kill');
+    
+    Route::resource('/post', 'PostController');
+});
